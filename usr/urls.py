3754c441bf7d36d537e7580fce461cfc67739e25
xs143 <Sxw123456!>
2023-02-04 15:44:17 -0500

    rider_order_detail

--- usr/urls.py
+++ usr/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import UsrProfileView
+from .views import UsrProfileView, usr_order_detail_view
 
 app_name = 'usr'
 
 urlpatterns = [
-  path('<int:id>/', UsrProfileView.as_view() , name='usr-profile'),
+  path('<int:id>/profile', UsrProfileView.as_view() , name='usr-profile'),
+  path('<int:id>/orderDetail', usr_order_detail_view, name='usr-order-detail')
+
 ]
--- usr/views.py
+++ usr/views.py
@@ -7,7 +7,7 @@ from django.views.generic import (
   DeleteView
 )
 
-from main.models import Profile
+from main.models import (Profile, Order,User)
 # Create your views here.
 
 class UsrProfileView(DetailView):
@@ -16,3 +16,31 @@ class UsrProfileView(DetailView):
   def get_object(self):
     id_ = self.kwargs.get("id")
     return get_object_or_404(Profile, id=id_) 
+
+
+def usr_order_detail_view(request,id):
+
+  try:
+    orders = Order.objects.get(id=id)
+  except Profile.DoesNotExist:
+    orders = None
+
+  try:
+    user = User.objects.get(username=request.user.username)
+    profile = Profile.objects.get(user=user)
+  except User.DoesNotExist:
+    user = None
+    profile = None
+    
+  context = {
+    'user': user,
+    'profile': profile,
+    'orders' : orders,
+
+  }
+  return render(request, 'usr/order_detail.html', context)
+
+  
+  
+  
+  
